ed0a3b45f9ff372ee3d7d4fbe883c98fd25ae8bf
NIYOBYOSE Isaac Precieux <isaprecieux112@gmail.com>
2026-02-21 11:34:25 +0200

    Clarify prime number check with a comment

--- Prime.py
+++ Prime.py
@@ -2,7 +2,7 @@ print ("All prime numbers between 1 and 100 are")
 for num in range(1, 101):
     if num > 1:
         for i in range(2, num):
-            if (num % i) == 0:
+            if (num % i) == 0:  # Are u printing prime numbers or odd numbers?????????????????????
                 break
         else:
             print(num)
